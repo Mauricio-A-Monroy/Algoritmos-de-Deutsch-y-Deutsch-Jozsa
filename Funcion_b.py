3662f5b72c43dcfd724f2b0f7292b5a2d8e0cedf
Mauricio-A-Monroy <alejomonry@hotmail.com>
2022-11-27 22:17:24 -0500

    commit

---
+++ Funcion_b.py
@@ -0,0 +1,130 @@
+import numpy as np
+from qiskit import QuantumCircuit, transpile
+from qiskit import Aer
+from qiskit.visualization import plot_histogram
+import matplotlib.pyplot as plt
+simulator = Aer.get_backend('qasm_simulator')
+
+matriz=[[0 for i in range (4)] for i in range (4)]
+
+"circuito para la entrada 00 "
+circuit = QuantumCircuit(2, 1)
+circuit.barrier(0,1)
+circuit.i(0)
+circuit.i(1)
+circuit.barrier(0,1)
+circuit.measure([1],[0])
+
+compiled_circuit = transpile(circuit, simulator)
+job = simulator.run(compiled_circuit, shots=1000)
+result = job.result()
+counts = result.get_counts(circuit)
+
+colum_0="00"
+row_0="0"+str(list(counts.keys())[0])
+
+#Pasar de numeros binarios a naturales para agregar completar la matriz
+matriz[int(row_0,2)][int(colum_0,2)]=1
+
+print("Resultado para "+colum_0+": "+row_0)
+print(circuit)
+
+"circuito para la entrada 01 "
+circuit = QuantumCircuit(2, 1)
+circuit.x(1)
+circuit.barrier(0,1)
+circuit.i(0)
+circuit.i(1)
+circuit.barrier(0,1)
+circuit.measure([1],[0])
+
+compiled_circuit = transpile(circuit, simulator)
+job = simulator.run(compiled_circuit, shots=1000)
+result = job.result()
+counts = result.get_counts(circuit)
+
+colum_1="01"
+row_1="0"+str(list(counts.keys())[0])
+
+#Pasar de numeros binarios a naturales para agregar completar la matriz
+matriz[int(row_1,2)][int(colum_1,2)]=1
+
+print("Resultado para 01:","0"+str(list(counts.keys())[0]))
+print(circuit)
+
+"circuito para la entrada 10 "
+circuit = QuantumCircuit(2, 1)
+circuit.x(0)
+circuit.barrier(0,1)
+circuit.i(0)
+circuit.i(1)
+circuit.barrier(0,1)
+circuit.measure([1],[0])
+
+compiled_circuit = transpile(circuit, simulator)
+job = simulator.run(compiled_circuit, shots=1000)
+result = job.result()
+counts = result.get_counts(circuit)
+
+colum_2="10"
+row_2="1"+str(list(counts.keys())[0])
+
+#Pasar de numeros binarios a naturales para agregar completar la matriz
+matriz[int(row_2,2)][int(colum_2,2)]=1
+
+print("Resultado para 10:","1"+str(list(counts.keys())[0]))
+print(circuit)
+
+"circuito para la entrada 11 "
+circuit = QuantumCircuit(2, 1)
+circuit.x(0)
+circuit.x(1)
+circuit.barrier(0,1)
+circuit.i(0)
+circuit.i(1)
+circuit.barrier(0,1)
+circuit.measure([1],[0])
+
+compiled_circuit = transpile(circuit, simulator)
+job = simulator.run(compiled_circuit, shots=1000)
+result = job.result()
+counts = result.get_counts(circuit)
+
+colum_3="11"
+row_3="1"+str(list(counts.keys())[0])
+
+#Pasar de numeros binarios a naturales para agregar completar la matriz
+matriz[int(row_3,2)][int(colum_3,2)]=1
+
+print("Resultado para 11:","1"+str(list(counts.keys())[0]))
+print(circuit)
+
+print("La matriz correspondiente del circuito")
+for i in matriz:
+    print(*i)
+
+#Resultado del algoritmo Deutsch en la función a
+
+circuit = QuantumCircuit(2, 1)
+
+circuit.x(1)
+circuit.barrier(0,1)
+circuit.h(0)
+circuit.h(1)
+circuit.barrier(0,1)
+
+#función b
+circuit.i(0)
+circuit.i(1)
+
+circuit.barrier(0,1)
+circuit.h(0)
+circuit.measure([0], [0])
+
+compiled_circuit = transpile(circuit, simulator)
+job = simulator.run(compiled_circuit, shots=1000)
+result = job.result()
+counts = result.get_counts(circuit)
+
+print("\nResultado del algoritmo Deutsch para la función b:",list(counts.keys())[0])
+print(circuit)
---
+++ Readme.md
@@ -0,0 +1,20 @@
+# Libreria del algoritmo de Deutsch y Deutsch-Jozsa
+#### En esta librería se encuentra 7 códigos, los 4 primeros (Funcion_a, Funcion_b, Funcion_c y Funcion_d) corresponden a las funciones que pueden ser utilizadas para usar el algoritmo de Deutsch, las otras 3 (funcion_a2, funcion_b2 y funcion_c2) don funciones que pueden utilizarse en el algoritmo de Deutsch-Jozsa. Los 7 códigos tienen la siguiente estructura:
+- Creación de la matriz correspondiente a la función.
+- Creación del circuito para cada uno de los casos.
+- Llenar la matriz con unos en las posiciones correctas.
+- Imprimir el circuito con su resultado.
+- Imprimir la matriz del circuito.
+- Implementación de los algoritmode de Deutsch y Deutsch-Jozsa según corresponda.
+- Implimir el circuito del algoritmo correspondiente y su resultado.
+
+
+## Construido con
+- [PyCharm](https://www.jetbrains.com/pycharm/) - IDE de Python usado
+## Autores
+- **Mauricio Monroy** - [*Mauricio-A-Monroy*](https://github.com/Mauricio-A-Monroy)
+## Agradecimientos
+- https://qastack.mx/programming/10411085/converting-integer-to-binary-in-python
+- https://qiskit.org/documentation/intro_tutorial1.html
+- https://qiskit.org/documentation/stubs/qiskit.circuit.QuantumCircuit.toffoli.html
+- https://www.youtube.com/watch?v=RXfmP7eaV5s
